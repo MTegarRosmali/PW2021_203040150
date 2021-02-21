2f2a75f1794f99fc67505b147505cdf484a31f45
MTegarRosmali <02tegarrosmali@gmail.com>
2021-02-20 16:20:35 -0800

    menyelesaikan pertemuan3

---
+++ pertemuan3/latihan1.php
@@ -0,0 +1,30 @@
+<?php 
+/*
+M.Tegar Nurul Fuad Rosmali
+203040150
+https://github.com/MTegarRosmali/pw2021_203040150
+Pertemuan 2 - 12 Februari 2021
+Mempelajari Sintaks PHP Dasar
+*/
+?>
+<?php
+// Pengulangan
+// for
+// while
+// do.. while
+// foreach : pengulangan khusus array
+
+// for ( $i = 0; $i < 5; $i++) {
+// 	echo "hello world! <br>";
+// }
+// $i = 10;
+// while($i <5 ) {
+// 	echo "Hello World! <br>";
+// 	$i++;
+// }
+// $i = 10;
+// do {
+// 	echo "Hello World! <br>";
+// $i++;
+// }while($i < 5);
+ ?>
---
+++ pertemuan3/latihan3.php
@@ -0,0 +1,26 @@
+<?php 
+/*
+M.Tegar Nurul Fuad Rosmali
+203040150
+https://github.com/MTegarRosmali/pw2021_203040150
+Pertemuan 2 - 12 Februari 2021
+Mempelajari Sintaks PHP Dasar
+*/
+?>
+<?php 
+// Pengkondisian / Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+// $x = 2;
+// if( $x < 20 ) {
+// 	echo "benar";
+// } else if ( $x == 20 ) {
+// 	echo "bingo!";
+// } else {
+// 	echo "salah";
+// }
+
+ ?>
